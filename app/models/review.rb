dc144070eff2a17b8fcea1cb20c75bbf0a9f508c
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-16 16:23:47 +0200

    correct mistakes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking, :dependent => :destroy
 
+  validates :comment, presence: true
   validates :rating, presence: true
-  validates :rating, inclusion: { in: (0..10), message: "%{value} is not a valid rating" }
+  validates :rating, inclusion: { in: (0..10), message: "%{value} is not a valid rating, it has to be between 0 and 10" }
 end

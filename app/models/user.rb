e0cbeeab9024ff544652f51e7d10619c82f97550
Benedikt Nolte <bene_nolte@me.com>
2021-08-16 16:23:30 +0200

    Validation for User and  Dog

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :dogs
+  has_many :bookings
+
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :street, presence: true
+  validates :city, presence: true
+  validates_format_of :zip_code, :with => /^\d{5}(-\d{4})?$/, :message => "Zip code should be valid"
 end

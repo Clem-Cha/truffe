7f343d320b28667f87c076d3dacf6bb654a0fc8e
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-16 18:02:30 +0200

    erase user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,4 @@ class User < ApplicationRecord
 
   has_many :dogs
   has_many :bookings
-
-  # Validations
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :street, presence: true
-  validates :city, presence: true
-  validates_format_of :zipcode, :with => /\A\d{5}(-\d{4})?\z/, :message => "Zip code should be valid"
 end

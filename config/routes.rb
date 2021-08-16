40cdcc11ad344584b5e1dbb0bf17141c3c931dec
Benedikt Nolte <bene_nolte@me.com>
2021-08-16 15:48:13 +0200

    Setup our initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dogs, only [ :show, :index, :new, :create, :edit, :update ] do
+    resources :bookings, only: [ :create ]
+  end
+  resources :bookings, only: [ :show, :edit, :update ] do
+    resources :reviews, only: [ :create ]
+  end
 end

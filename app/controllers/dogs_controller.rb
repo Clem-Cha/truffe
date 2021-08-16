f1361e4a72ff923b7196cd4000adebf4844ac094
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-16 16:43:59 +0200

    Created Dogs controller & associated view pages

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,38 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+     @dog = Dog.new(dog_params)
+     @dog.user = current_user
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+    redirect_to dogs_path(@dog)
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed, :category, :description)
+  end
+end

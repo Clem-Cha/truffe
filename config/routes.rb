abd9653ad1f33a867319bc4660d05ca5273ffe58
Benedikt Nolte <bene_nolte@me.com>
2021-08-16 15:50:51 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dogs, only [ :show, :index, :new, :create, :edit, :update ] do
+  resources :dogs, only: [ :show, :index, :new, :create, :edit, :update ] do
     resources :bookings, only: [ :create ]
   end
   resources :bookings, only: [ :show, :edit, :update ] do

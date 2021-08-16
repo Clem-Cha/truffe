baea374d7eb0ec9df7f92e3ff99a3dbc5ebdedc6
Benedikt Nolte <bene_nolte@me.com>
2021-08-16 18:04:31 +0200

    Added controller + view for bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,21 @@
+class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:dog_id, :user_id, :start_date, :end_date, :status)
+  end
+end

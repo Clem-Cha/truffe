750c232a480c2821c36a617a5a99e98c0e045ee0
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-16 16:26:40 +0200

    correct mistakes again

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :dog, :dependent => :destroy
   belongs_to :user, :dependent => :destroy
-  has_many :reviews
 
   validates :start_date, presence: true
   validates :end_date, presence: true

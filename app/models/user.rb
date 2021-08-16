a1389ed3e9952f76b468b461f3a674da2891b834
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-16 18:41:18 +0200

    added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :dogs
-  has_many :bookings
+  has_many :dogs, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end

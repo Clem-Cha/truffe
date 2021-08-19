414aa9b73614e30d8926c0e358e510ac1ddf62b7
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-19 11:58:25 +0200

    Init dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,14 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @dogs = policy_scope(Dog)
+    @dogs = Dog.where(user_id: current_user)
+
+
+    @bookings = policy_scope(Booking)
+    @bookings_pending = @bookings.select { |booking| booking.status == "pending" }
+    @bookings_history = @bookings.select { |booking| booking.status == "approved" || booking.status == "rejected" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'bookings/show'
   devise_for :users
   root to: 'pages#home'
+  get "dashboard", to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :dogs, only: [ :show, :index, :new, :create, :edit, :update ] do
     resources :bookings, only: [ :create ]

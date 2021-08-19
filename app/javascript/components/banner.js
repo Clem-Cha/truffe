bae5450653dc2023c73dd0e570712563b20e3b14
Lydia Seyler <lydia@inskip.fr>
2021-08-19 14:36:27 +0200

    categories added

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,8 +1,9 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
+  console.log('hello');
   new Typed('#banner-typed-text', {
-    strings: ["Find the right dog to enlighten any moment of your life"],
+    strings: ["Find the right dog to enlighten any moment of your life 🐶"],
     typeSpeed: 50,
     loop: true
   });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,23 +23,14 @@ require("channels")
 
 // External imports
 import "bootstrap";
-// Map import
-import { initMapbox } from '../plugins/init_mapbox';
-import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { loadDynamicBannerText } from "../components/banner";
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
   initMapbox();
   initUpdateNavbarOnScroll();
-});
-
-// Adding dynamic tagline on homepage
-import { loadDynamicBannerText } from '../components/banner';
-
-document.addEventListener('turbolinks:load', () => {
   loadDynamicBannerText();
 });

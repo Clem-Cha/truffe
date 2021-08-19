e4e564485e1d54fb1d5d7d99048d7960850662cd
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-19 14:10:58 +0200

    done edit and destroy bookings

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -5,11 +5,19 @@ class BookingPolicy < ApplicationPolicy
     end
   end
 
+  def new?
+    create?
+  end
+
   def create?
     return true
   end
 
   def update?
+    record.user == user
+  end
+
+  def destroy?
     record.user == user
   end
 end

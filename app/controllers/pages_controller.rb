a0fe4ebf355b00660e7d382fb247d303abaa6918
Lydia Seyler <lydia@inskip.fr>
2021-08-19 16:25:45 +0200

    add rating

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @top3_dogs = Dog.joins(:bookings, :reviews)
+                    .select("dogs.id, avg(reviews.rating) as average_rating")
+                    .group("dogs.id")
+                    .order("average_rating DESC")
+                    .limit(3)
+
+    @dogs = Dog.where(id: @top3_dogs)
+
+    raise
+
   end
 end

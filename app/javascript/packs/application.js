349aea1baf371f982ed75d9ffc4959d2b73f3a24
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-19 11:21:19 +0200

    add js to navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("channels")
 import "bootstrap";
 // Map import
 import { initMapbox } from '../plugins/init_mapbox';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -34,4 +35,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initUpdateNavbarOnScroll();
 });

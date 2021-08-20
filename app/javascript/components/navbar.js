c891e2c1d07b84380d52dbb3bfca7b0fce89459e
Benedikt Nolte <bene_nolte@me.com>
2021-08-20 11:19:47 +0200

    Fixed navbar on home and added functionality

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,8 +4,17 @@ const initUpdateNavbarOnScroll = () => {
 
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 240 && document.location.pathname === "/") {
-        console.log("hello");
+      if (window.scrollY >= 660 && document.location.pathname === "/") {
+        navbar.classList.remove('navbar-truffe-transparent');
+        buttons.forEach((button) => {
+          button.style.color = "rgba(0, 0, 0, 0.55)";
+        });
+      } else if (window.scrollY >= 470 && document.location.pathname === "/") {
+        navbar.classList.add('navbar-truffe-transparent');
+        buttons.forEach((button) => {
+          button.style.color = "white";
+        });
+      } else if (window.scrollY >= 360 && document.location.pathname === "/") {
         navbar.classList.remove('navbar-truffe-transparent');
         buttons.forEach((button) => {
           button.style.color = "rgba(0, 0, 0, 0.55)";

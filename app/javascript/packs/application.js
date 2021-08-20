a06e97325b025e4ac13ae266f286421fcc8ac12b
Clemence Chalot <clemence.chalot@gmail.com>
2021-08-20 12:30:10 +0200

    Added flatpickr on forms
    Signed-off-by: Clemence Chalot <clemence.chalot@gmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,7 @@ import { initUpdateSearchbarShowOnScroll } from '../components/searchbar-show';
 import { initReviewOnHover } from '../components/review';
 import { loadDynamicBannerText } from "../components/banner";
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { initFlatpickr } from "../plugins/flatpickr";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -44,4 +45,5 @@ document.addEventListener('turbolinks:load', () => {
   initUpdateSearchbarOnScroll();
   initUpdateSearchbarShowOnScroll();
   initAutocomplete();
+  initFlatpickr();
 });
